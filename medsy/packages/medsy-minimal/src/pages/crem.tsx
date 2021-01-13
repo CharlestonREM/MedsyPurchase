562bb97176630c1180c0122a288d80716539b55b
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-01-13 10:32:52 -0500

    change structure of onSubmit async to match postData function

--- medsy/packages/medsy-minimal/src/pages/crem.tsx
+++ medsy/packages/medsy-minimal/src/pages/crem.tsx
@@ -161,30 +161,25 @@ export default function Crem({ products }) {
         sessionSpecialRequests: ''
     }
     //> onSubmit handler to pass to Formik component
-    const onSubmit = (values, { setSubmitting }) => {
+    const onSubmit = async (values, { setSubmitting }) => {
         console.log('submit button was clicked.VALUES:', values);
-        async () => {
+        //employ javascripts global fetch method for easy,logical way to fetch resources asynchronously across the network
+        //employ the await operator to wait for the returned promise; the await operator is used inside the async function started above because this is the only context the operator can be used. the syntax is [rv] await expression; so it awaits an expression that is a Promise or any value to wait for. the rv is the returned value;  it returns the fulfilled value of the promise, or the value itself if its not the promise
+        const res = await fetch('/api/postData', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+        });
+        console.log('i am res in postData:')
+        console.log(res);
+        if (res.status === 200) {
+            setSuccess(true);
 
-
-
-            //employ javascripts global fetch method for easy,logical way to fetch resources asynchronously across the network
-            //employ the await operator to wait for the returned promise; the await operator is used inside the async function started above because this is the only context the operator can be used. the syntax is [rv] await expression; so it awaits an expression that is a Promise or any value to wait for. the rv is the returned value;  it returns the fulfilled value of the promise, or the value itself if its not the promise
-            const res = await fetch('/api/postData', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
-            });
-            console.log('i am res in postData:')
-            console.log(res);
-            if (res.status === 200) {
-                setSuccess(true);
-
-            } else {
-                //setError(true);
-            }
-        };
+        } else {
+            //setError(true);
+        }
         // setTimeout(() => {
         //     setSubmitting(false);
         //     alert(JSON.stringify(values, null, 2));
