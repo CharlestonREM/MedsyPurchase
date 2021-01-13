6a2eb3cc0f7ffee31a6aba90da65eca7d1dde43d
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-01-13 10:25:58 -0500

    refactor formik fields in crem.tsx; refactor onSubmit function to be handled by formik and its `values` property

--- medsy/packages/medsy-minimal/src/pages/crem.tsx
+++ medsy/packages/medsy-minimal/src/pages/crem.tsx
@@ -1,11 +1,11 @@
 //useEffect info:
 import { useEffect } from 'react';
-import { Card, CardContent, Typography, Button } from '@material-ui/core';
-import { Field, Form, Formik } from 'formik';
+import { Card, CardContent, Typography, Button, LinearProgress, FormControl, FormHelperText } from '@material-ui/core';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
 
 //radio imports
-import { FormControlLabel, Radio, LinearProgress } from '@material-ui/core';
+import { FormControlLabel, Radio } from '@material-ui/core';
 import { RadioGroup } from 'formik-material-ui';
 
 //products
@@ -23,12 +23,21 @@ import { useSearch } from 'contexts/search/use-search';
 //use state
 import { useState, useContext } from 'react';
 
+//import the order-submit component
+// info This is the component that is returned when the success state changes to true
+// info --- The success state with the initial state of false changes to true
+
+import OrderSubmit from 'containers/drawer/views/order-submit';
+
 
 //set initial state of the form values to be an argument in the useState hook
 const initialState = {
     propertyType: '',
     propertySize: ''
 };
+
+
+
 
 //component
 //---> you have to pass in `products` as a parameter of the component, i.e. as arbitrary arguments, i.e. as props!
@@ -65,12 +74,22 @@ export default function Crem({ products }) {
 
     // info -- set onChange and setFormDAta functions
     const onChange = (e) => {
+        console.log('onChange function executed.')
+        console.log('here is form data BEFORE CHANGE IS REGISTERED...')
+        console.log(formData)
+        console.log('e.target:')
+        console.log(e.target);
         const { value, name } = e.currentTarget;
         setFormData({
             ...formData,
             [name]: value,
         });
+        console.log('formData after setFormData:')
+        console.log(formData)
     };
+    if (success) {
+        return <OrderSubmit />;
+    }
     console.log(products)
 
 
@@ -106,62 +125,199 @@ export default function Crem({ products }) {
         }
     };
 
+    /* 
+    ! -----------------------
+    ! FORMIK CODE BEGINS
+    ! -----------------------
+    info I am using Formik to set up the form that will send data to the google spreadsheet and formik-material-ui to link material-ui user interfac library to formiks form control
+ */
+    //> set up initialValues parameter for Formik component
+    //  >Formik will make these values available to render methods component as `values`
+    const initialValues = {
+        propertyType: '',
+        propertySize: '',
+        baseServiceCheckbox: '',
+        basePackageCheckbox: '',
+        upgradeCheckbox: '',
+        confirmSelectionCheckbox: '',
+        customerName: '',
+        brokerage: '',
+        email: '',
+        phone: '',
+        propertyStreetAddress: '',
+        propertyCity: '',
+        propertyState: '',
+        propertyZip: '',
+        propertyOccupancy: '',
+        propertyGateCode: '',
+        propertyPets: '',
+        propertyLockCode: '',
+        propertySpecialRequests: '',
+        sessionPreferredDate: '',
+        sessionPreferredTime: '',
+        sessionAlternateDate: '',
+        sessionAlternateTime: '',
+        licenseType: 'single',
+        sessionSpecialRequests: ''
+    }
+    //> onSubmit handler to pass to Formik component
+    const onSubmit = (values, { setSubmitting }) => {
+        console.log('submit button was clicked.VALUES:', values);
+        async () => {
+
+
+
+            //employ javascripts global fetch method for easy,logical way to fetch resources asynchronously across the network
+            //employ the await operator to wait for the returned promise; the await operator is used inside the async function started above because this is the only context the operator can be used. the syntax is [rv] await expression; so it awaits an expression that is a Promise or any value to wait for. the rv is the returned value;  it returns the fulfilled value of the promise, or the value itself if its not the promise
+            const res = await fetch('/api/postData', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values),
+            });
+            console.log('i am res in postData:')
+            console.log(res);
+            if (res.status === 200) {
+                setSuccess(true);
+
+            } else {
+                //setError(true);
+            }
+        };
+        // setTimeout(() => {
+        //     setSubmitting(false);
+        //     alert(JSON.stringify(values, null, 2));
+        // }, 500);
+    }
+
 
     return (
         <Card>
             <CardContent>
                 <Products items={products} ref={elRef} />
 
-                <Formik initialValues={{
-                    propertyType: '',
-                    propertySize: '',
-                    baseServiceCheckbox: '',
-                    basePackageCheckbox: '',
-                    upgradeCheckbox: '',
-                    confirmSelectionCheckbox: '',
-                    customerName: '',
-                    brokerage: '',
-                    email: '',
-                    phone: '',
-                    propertyStreetAddress: '',
-                    propertyCity: '',
-                    propertyState: '',
-                    propertyZip: '',
-                    propertyOccupancy: '',
-                    propertyGateCode: '',
-                    propertyPets: '',
-                    propertyLockCode: '',
-                    propertySpecialRequests: '',
-                    sessionPreferredDate: '',
-                    sessionPreferredTime: '',
-                    sessionAlternateDate: '',
-                    sessionAlternateTime: '',
-                    licenseType: '',
-                    sessionSpecialRequests: ''
-                }} onSubmit={() => { }}>
-                    <Form>
-                        <Field component={RadioGroup} name="propertyType">
-                            <FormControlLabel
-                                value={products[0].name}
-                                control={<Radio />}
-                                label={products[0].name}
-                            />
-                            <FormControlLabel
-                                value="land"
-                                control={<Radio />}
-                                label="Land"
-                            />
-                        </Field>
-                        <Field name="propertySize" component={TextField} label="Property Size" />
-                        <div className="flex flex-col p-30px">
-                            <Button className="big w-full" onClick={postData} >
-                                Order Now
-        </Button>
-                        </div>
-                    </Form>
+                <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                    {/* 
+                    //? Getting Started docs has me passing submitForm variable as parameter in an anonymous function
+                        //?is it an anonymous render function?
+                     */}
+                    {({ submitForm, isSubmitting }) => (
+                        <Form>
+                            {/* 
+                                //info place fields here from initial values
+                             */}
+
+                            <FormControl>
+                                <Field component={RadioGroup} name="propertyType">
+                                    <FormHelperText>Is the property we're shooting land or a house?</FormHelperText>
+                                    <FormControlLabel
+                                        value={products[0].name}
+                                        control={<Radio />}
+                                        label={products[0].name}
+                                    />
+                                    <FormControlLabel
+                                        value="land"
+                                        control={<Radio />}
+                                        label="Land"
+                                    />
+                                </Field>
+                                <ErrorMessage name="propertyType" />
+                            </FormControl>
+
+                            <FormControl>
+                                <Field name="propertySize" component={TextField} label="Property Size" />
+                                {isSubmitting && <LinearProgress />}
+                            </FormControl>
+
+                            <FormControl>
+                                <Field name="baseServiceCheckbox" label="Select your base service" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="basePackageCheckbox" label="Select your base package" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="upgradeCheckbox" label="Select your upgrade" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="confirmSelectionCheckbox" label="Confirm your selection" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="customerName" label="Name" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="brokerage" label="Name of Brokerage (optional)" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="email" label="Email address" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="phone" label="Phone Number" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyStreetAddress" label="Street Address" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyCity" label="City" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyState" label="State" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyZip" label="Zip Code" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyOccupancy" label="Property Occupancy" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyGateCode" label="Gate Code (if applicable)" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyPets" label="Do you have pets?" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertyLockCode" label="Lock Code (if applicable)" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="propertySpecialRequests" label="Special Requests (e.g.)" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="sessionPreferredDate" label="Preferred Date" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="sessionPreferredTime" label="Preferred Time" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="sessionAlternateDate" label="Alternate Date" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="sessionAlternateTime" label="Alternate Time" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="licenseType" label="Select your license type" component={TextField} />
+                            </FormControl>
+                            <FormControl>
+                                <Field name="sessionSpecialRequests" label="Special Requests (e.g.)" component={TextField} />
+                            </FormControl>
+
+
+                            {/* 
+                            //info the Button
+                             */}
+
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                disabled={isSubmitting}
+                                onClick={submitForm}
+                            >
+                                Submit
+                            </Button>
+                        </Form>
+                    )}
                 </Formik>
             </CardContent>
-        </Card>
+        </Card >
     )
 }
 
