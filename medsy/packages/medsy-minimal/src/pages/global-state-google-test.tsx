876da4bc7e05454a2df2ac8331df507fc07049ee
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-03-08 05:46:05 -0500

    create global state library test to push to vercel

---
+++ medsy/packages/medsy-minimal/src/pages/global-state-google-test.tsx
@@ -0,0 +1,13 @@
+export interface GlobalStateGoogleTestProps {
+
+}
+
+const GlobalStateGoogleTest: React.FC<GlobalStateGoogleTestProps> = () => {
+    return (
+        <div>
+            <h1>Global State Google Test</h1>
+        </div>
+    );
+}
+
+export default GlobalStateGoogleTest;
