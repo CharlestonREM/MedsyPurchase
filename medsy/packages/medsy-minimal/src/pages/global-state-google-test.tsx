a50073a3208cb3b5240b367c668751129c02cc96
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-03-09 06:49:45 -0500

    comment out globalState refs in global-state-googletest.tsx for vercel 500 error debugging

--- medsy/packages/medsy-minimal/src/pages/global-state-google-test.tsx
+++ medsy/packages/medsy-minimal/src/pages/global-state-google-test.tsx
@@ -1,37 +1,37 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 
-import {
-    useAsyncData,
-    useGlobalState,
-} from '@dr.pogodin/react-global-state';
+// import {
+//     useAsyncData,
+//     useGlobalState,
+// } from '@dr.pogodin/react-global-state';
 
-/* Example of component relying on the global state. */
+// /* Example of component relying on the global state. */
 
-function SampleComponent() {
-    const [value, setValue] = useGlobalState('sample.component', 0);
-    return (
-        <div>
-            <h1>`SampleComponent`</h1>
-            <button onClick={() => setValue(1 + value)}>
-                {value}
-            </button>
-        </div>
-    );
-}
+// function SampleComponent() {
+//     const [value, setValue] = useGlobalState('sample.component', 0);
+//     return (
+//         <div>
+//             <h1>`SampleComponent`</h1>
+//             <button onClick={() => setValue(1 + value)}>
+//                 {value}
+//             </button>
+//         </div>
+//     );
+// }
 
 /* Example of component relying on async data in the global state. */
 
-async function sampleDataLoader() {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(<Typography variant="h2">I am sample data</Typography>), 900);
-    });
-}
+// async function sampleDataLoader() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(<Typography variant="h2">I am sample data</Typography>), 900);
+//     });
+// }
 
-function SampleAsyncComponent() {
-    const { data, loading } = useAsyncData('sample.async-component', sampleDataLoader);
-    return data;
-}
+// function SampleAsyncComponent() {
+//     const { data, loading } = useAsyncData('sample.async-component', sampleDataLoader);
+//     return data;
+// }
 
 
 
@@ -46,8 +46,8 @@ const GlobalStateGoogleTest: React.FC<GlobalStateGoogleTestProps> = () => {
             <div>
                 <h1>Global State Google Test</h1>
             </div>
-            <SampleComponent />
-            <SampleAsyncComponent />
+            {/* <SampleComponent />
+            <SampleAsyncComponent /> */}
         </React.Fragment>
     );
 }
