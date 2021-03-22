2072e00f9b928741c37791bc5eb3b216d5b0abff
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-03-22 10:44:07 -0400

    readd baspackageaccordioncheckboxgropu component in selectebaseproductstep component; remove the css const and the style element

--- medsy/packages/medsy-minimal/src/components/select-base-products-step.tsx
+++ medsy/packages/medsy-minimal/src/components/select-base-products-step.tsx
@@ -51,7 +51,7 @@ const SelectBaseProductsStep: React.FC<SelectBaseProductsStepProps> = (props) =>
                         <Typography style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', fontWeight: 700, fontSize: '1.15rem', margin: '2rem auto', color: '#999999' }}><span>{getServiceIcon(service)}</span><span>{getServiceData(service).name}</span><span style={{ color: 'black' }}>/Base Packages</span></Typography>
                     </Grid>
                     <Grid container>
-                        {/* <BasePackageAccordionCheckboxGroup fieldName="basePackageCheckbox" label="i am bp accordion label" service={service} basePackages={servicePackages} /> */}
+                        <BasePackageAccordionCheckboxGroup fieldName="basePackageCheckbox" label="i am bp accordion label" service={service} basePackages={servicePackages} />
                     </Grid>
                 </Grid>
             </React.Fragment>
